19957f41ad33cf0c4053ef9981ab53062943158a
OtonielJacobo <115114972+OtonielJacobo@users.noreply.github.com>
2026-07-21 09:16:37 -0700

    edit

--- 22400559_Berumen_Jacobo_Cesar_Otoniel/U4/apiNetflix/index.js
+++ 22400559_Berumen_Jacobo_Cesar_Otoniel/U4/apiNetflix/index.js
@@ -1,25 +1,10 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-const html = `<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>api de pleiculas y series</title>
-</head>
-<body>
-  <header>
-    <h1>api de pleiculas y series</h1>
-  </header>
-</body>
-</html>`;
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
-  res.end(html);
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
+app.get("/pagina", (req, res) => {
+    res.send(`
+            <h1>mi página web </h1>
+            <p style="color:blue;">API de peliculas y series</p>
+        `)
+})
